a0762c144b5c4cb59fc1ad597b067f1e6d4ee5c5
DenRen <denrenruslan@gmail.com>
2019-10-27 19:25:21 +0300

    Отклонена идея совмещения кругого буфера. Список готов. Есть графический дамп в папке dot_dir

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <MyLib.h>
 #include <Super_Stack_Library.h>
+#include <cstdlib>
 
 namespace lst {
     typedef __int32_t data_t;
@@ -9,31 +10,32 @@ namespace lst {
     enum err_constants {
         ALL_RIGHT,
         CONSTR_EMPTY_SIZE,
+        INCORRECT_EXPN,
         CALLOC_ERR,
+        REALLOC_ERR,
         NO_ELEMENT
     };
 
+    bool dmp_DOT_ON = true;
+    bool dmp_DOT_SHOW_AT_ONCE = false;
+    bool dmp_Creat_Animatoin = true;
 }
-
 
 struct list_t {
     lst::num_t size;    // Максимальное возможное кол-во элементов в списке
+    double expn;        // Коэффициент расширения
     lst::data_t *data;  // Указатель непосредственно на данные
-    lst::num_t *next;  // Служебный массив номеров следующих элементов
-    lst::num_t *prev;  // Служебный массив номеров предыдущих элементов
-    lst::num_t head;   // Номер первого элемента в списке (если 0, то список пуст)
-    lst::num_t tail;   // Номер последнего элемента в списке (если 0, то список пуст, если head, то 1 элемент в списке)
-    lst::num_t free;   // Первый номер свободной ячейки в списке
-    lst::num_t count;  // Количество занятых ячеек
+    lst::num_t *next;   // Служебный массив номеров следующих элементов
+    lst::num_t *prev;   // Служебный массив номеров предыдущих элементов
+    lst::num_t head;    // Номер первого элемента в списке (если 0, то список пуст)
+    lst::num_t tail;    // Номер последнего элемента в списке (если 0, то список пуст, если head, то 1 элемент в списке)
+    lst::num_t free;    // Первый номер свободной ячейки в списке
+    lst::num_t count;   // Количество занятых ячеек
 };
 
 int list_Construct (list_t *list);
 
 void list_Destruct (list_t *list);
-
-lst::num_t real2phys (lst::num_t number);
-
-lst::num_t phys2real (lst::num_t number);
 
 lst::num_t add_right (list_t *list, lst::data_t value, lst::num_t number, __uint8_t *state_func);
 
@@ -41,15 +43,28 @@ lst::num_t add_left (list_t *list, lst::data_t value, lst::num_t number, __uint8
 
 lst::num_t add_end (list_t *list, lst::data_t value, __uint8_t *state_func);
 
-lst::num_t del_elem (list_t *list, __uint8_t *state_func);
+lst::num_t add_begin (list_t *list, lst::data_t value, __uint8_t *state_func);
 
-lst::num_t find_elem (list_t *list, lst::num_t value, __uint8_t *state_func);
+void del_elem (list_t *list, lst::num_t number, __uint8_t *state_func);
 
-void Dump_list (list_t *list, FILE *file);
+lst::num_t find_elem (list_t *list, lst::num_t value);
+
+bool isfree (list_t *list, lst::num_t number, __uint8_t *state_func);
+
+void Dump_list (list_t *list, FILE *file = stdout);
+
+void Dump_list_dot (list_t *list);
+
+int checking_size_list (list_t *list);
+
+inline __uint64_t min_size_data (double expn) {
+    return (__uint64_t) (1 / expn) + 1;
+}
 
 int main () {
     struct list_t list = {};
-    list.size = 10;
+    list.size = 8;
+    list.expn = 1.2;
 
     if (list_Construct (&list) != lst::ALL_RIGHT) {
         list_Destruct (&list);
@@ -58,89 +73,192 @@ int main () {
 
     Dump_list (&list, stdout);
     __uint8_t state_func = 0;
-    for (int i = 10; i < 190; i += 10) {
+    for (int i = 10; i < 70; i += 10) {
         add_end (&list, i, &state_func);
         Dump_list (&list, stdout);
     }
 
+    del_elem (&list, 5, &state_func);
+    Dump_list (&list, stdout);
+
+    add_right (&list, 5, 3, &state_func);
+    Dump_list (&list, stdout);
+
+    add_right (&list, -57, 5, &state_func);
+    Dump_list (&list, stdout);
+    add_right (&list, 7, 5, &state_func);
+    Dump_list (&list, stdout);
+
+
+    add_end (&list, 50, &state_func);
+    Dump_list (&list, stdout);
+
+    add_left (&list, 56, 4, &state_func);
+    Dump_list (&list, stdout);
+
+    del_elem (&list, 1, &state_func);
+    Dump_list (&list, stdout);
+
+    printf ("%d", find_elem (&list, 7));
+
+
 
     list_Destruct (&list);
-
-
+    if (lst::dmp_Creat_Animatoin) {
+        system ("rm dot_dir/video.mp4 2> /dev/null; rm dot_dir/out.gif 2> /dev/null");
+        system ("ffmpeg -r 1 -i dot_dir/%d.png dot_dir/video.mp4 2> /dev/null");
+        system ("ffmpeg -i dot_dir/video.mp4 dot_dir/out.gif 2> /dev/null");
+    }
     return 0;
 }
 
-// Список очень гибкий и не выдаёт ошибки, если добавлять его некуда, а записывает в
-// первый логический элемент. Этим можно пользоваться очень хитро, но при этом
-// за "переполнением" должен следить программист, сравнивая list.count с list.size
-lst::num_t add_right (list_t *list, lst::data_t value, lst::num_t number, __uint8_t *state_func) {
+lst::num_t find_elem (list_t *list, lst::num_t value) {
+    lst::num_t ind = 0, tail = list->tail;
+    for (ind = list->head; ind != tail; ind = list->next[ind])
+        if (list->data[ind] == value)
+            return ind;
 
-    if (number == list->tail)
-        return add_end (list, value, state_func);  // Физ номер, куда был встален новый элемент
+    return list->data[ind] == value ? ind : -1;
+}
+
+bool isfree (list_t *list, lst::num_t number, __uint8_t *state_func) {
+    if (checking_size_list (list) != lst::ALL_RIGHT) {
+        *state_func = lst::REALLOC_ERR;
+        return -1;
+    }
+    lst::num_t temp_size = list->size, __free = 0;
+    for (__free = list->free; list->next[__free] != temp_size; __free = list->next[__free])
+        if (__free == number)
+            return true;
+    return __free == number;
+}
+
+lst::num_t add_right (list_t *list, lst::data_t value, lst::num_t number, __uint8_t *state_func) {
+    if (checking_size_list (list) != lst::ALL_RIGHT) {
+        *state_func = lst::REALLOC_ERR;
+        return -1;
+    } else if (number == list->tail) {
+        lst::num_t new_num = add_end (list, value, state_func);
+        return new_num;
+    }
 
     lst::num_t temp_free = list->free;
-    lst::num_t temp_tail = list->tail;
-
-    list->data[temp_free] = value;  //
-    list->tail = temp_free;         //
-
-    if (temp_free == list->head && list->count > 1)
-        list->head = list->next[temp_free];
-
     list->free = list->next[temp_free];
 
-    list->next[temp_free] = list->head;
+    list->data[temp_free] = value;
 
-    list->prev[temp_free] = temp_tail;
-    list->next[temp_tail] = temp_free;
+    lst::num_t temp_next = list->next[number];
+    list->next[number] = temp_free;
+    list->next[temp_free] = temp_next;
 
-    list->prev[list->head] = list->tail;
+    list->prev[temp_free] = number;
+    list->prev[temp_next] = temp_free;
 
     list->count++;
-    *state_func = lst::ALL_RIGHT;
-
-
-    lst::num_t temp = list->next[number];
-    list->next[number] = number_new;
-    list->next[number_new] = temp;
-
-
-    return number_new;
+    return temp_free;
 }
 
 lst::num_t add_end (list_t *list, lst::data_t value, __uint8_t *state_func) {
     // Спросить, что быстрее  list->data[list->free] = value или temp = list->free
     // list->data[temp] = value. Таких обращений к дате сотни. Если что, то переписать.
 
+    if (checking_size_list (list) != lst::ALL_RIGHT) {
+        *state_func = lst::REALLOC_ERR;
+        return -1;
+    }
+
     lst::num_t temp_free = list->free;
-    lst::num_t temp_tail = list->tail;
-
-    list->data[temp_free] = value;  //
-    list->tail = temp_free;         //
-
-    if (temp_free == list->head && list->count > 1)
-        list->head = list->next[temp_free];
-
     list->free = list->next[temp_free];
 
+    list->data[temp_free] = value;
     list->next[temp_free] = list->head;
+    list->prev[temp_free] = list->tail;
 
-    list->prev[temp_free] = temp_tail;
-    list->next[temp_tail] = temp_free;
-
-    list->prev[list->head] = list->tail;
+    list->next[list->tail] = temp_free;
+    list->tail = temp_free;
 
     list->count++;
-    *state_func = lst::ALL_RIGHT;
     return temp_free;
 }
 
+lst::num_t add_left (list_t *list, lst::data_t value, lst::num_t number, __uint8_t *state_func) {
+    if (checking_size_list (list) != lst::ALL_RIGHT) {
+        *state_func = lst::REALLOC_ERR;
+        return -1;
+    } else if (number == list->head) {
+        lst::num_t new_num = add_begin (list, value, state_func);
+        return new_num;
+    }
+
+    lst::num_t temp_free = list->free;
+    list->free = list->next[temp_free];
+
+    list->data[temp_free] = value;
+
+    list->next[temp_free] = number;
+    list->prev[temp_free] = list->prev[number];
+
+    list->next[list->prev[number]] = temp_free;
+    list->prev[number] = temp_free;
+
+    list->count++;
+    return temp_free;
+}
+
+lst::num_t add_begin (list_t *list, lst::data_t value, __uint8_t *state_func) {
+    // Спросить, что быстрее  list->data[list->free] = value или temp = list->free
+    // list->data[temp] = value. Таких обращений к дате сотни. Если что, то переписать.
+
+    if (checking_size_list (list) != lst::ALL_RIGHT) {
+        *state_func = lst::REALLOC_ERR;
+        return -1;
+    }
+
+    lst::num_t temp_free = list->free;
+    list->free = list->next[temp_free];
+
+    list->prev[list->head] = temp_free;
+
+    list->data[temp_free] = value;
+    list->next[temp_free] = list->head;
+    list->prev[temp_free] = temp_free;
+
+    list->head = temp_free;
+
+    list->count++;
+    return temp_free;
+}
+
+void del_elem (list_t *list, lst::num_t number, __uint8_t *state_func) {
+
+    if (number == list->tail)
+        list->tail = list->prev[number];
+    else if (number == list->head)
+        list->head = list->next[number];
+
+    list->next[list->prev[number]] = list->next[number];
+    list->prev[list->next[number]] = list->prev[number];
+
+    list->next[number] = list->free;
+    list->free = number;
+
+    list->count--;
+    *state_func = lst::ALL_RIGHT;
+}
+
 int list_Construct (list_t *list) {
-    // Выдаёт предупреждение, если указан желаемый некорректный размер
+    // Выдаёт предупреждение, если указан желаемый некорректный размер 11:30
     if (list->size <= 0) {
         PRINT_WARNING ("Constructor: Incorrect value size")
         return lst::CONSTR_EMPTY_SIZE;
+    } else if (list->expn <= 0) {
+        PRINT_ERROR ("Constructor: Incorrect value expansion")
+        return lst::INCORRECT_EXPN;
     }
+
+    if (list->size < min_size_data (list->expn))    // Проверка на минимальный размер массива списка
+        list->size = min_size_data (list->expn);
+
 
     // Контроллируется создание каждого массива
     if ((list->data = (lst::data_t *) calloc (list->size, sizeof (lst::data_t))) == nullptr) {
@@ -154,11 +272,12 @@ int list_Construct (list_t *list) {
         free (list->data);
     } else {    // Начальные значения переменных и массивов
         list->head = list->tail = 0;
-        list->free = list->count = 0;
+        list->free = 0;
+        list->count = 0;
+
 
         for (lst::num_t i = 0; i < list->size; i++)
             list->next[i] = i + 1;
-        list->next[list->size - 1] = 0;
 
         /*list->prev[0] = list->size - 1;
         for (lst::num_t i = 0; i < list->size - 1; i++)
@@ -187,14 +306,43 @@ void list_Destruct (list_t *list) {
     list->head = list->tail = 0;
 }
 
-inline lst::num_t real2phys (lst::num_t number) {
-    return number + 1;  // Написать для отрицательных номеров
-}
+int checking_size_list (list_t *list) {
+    if (list->count == list->size) {
+        lst::data_t *temp_data = nullptr;
+        lst::num_t *temp_next = nullptr;
+        lst::num_t *temp_prev = nullptr;
 
-inline lst::num_t phys2real (lst::num_t number) {
-    return number - 1;
-}
+        auto new_size = (lst::num_t) (((double) list->size) * (1 + list->expn));
 
+        if ((temp_data = (lst::data_t *) realloc (list->data, new_size * sizeof (lst::data_t))) == nullptr) {
+            PRINT_ERROR ("Failed to recreate data array. Clear the list")
+        } else if ((temp_next = (lst::num_t *) realloc (list->next, new_size * sizeof (lst::num_t))) == nullptr) {
+            PRINT_ERROR ("Failed to recreate next array. Clear the list")
+        } else if ((temp_prev = (lst::num_t *) realloc (list->prev, new_size * sizeof (lst::num_t))) == nullptr) {
+            PRINT_ERROR ("Failed to recreate prev array. Clear the list")
+        } else {    // Начальные значения переменных и массивов
+            list->data = temp_data;
+            list->next = temp_next;
+            list->prev = temp_prev;
+
+            for (lst::num_t i = list->size; i < new_size; i++) {
+                list->data[i] = 0;
+                list->next[i] = i + 1;
+                list->prev[i] = 0;
+            }
+
+            list->size = new_size;
+
+            /*list->prev[0] = list->size - 1;
+            for (lst::num_t i = 0; i < list->size - 1; i++)
+                list->prev[i + 1] = i;*/
+            return lst::ALL_RIGHT;
+            list->size = new_size;
+        }
+        return lst::REALLOC_ERR;
+    }
+    return lst::ALL_RIGHT;
+}
 
 void Dump_list (list_t *list, FILE *file) {
 /* IBM864
@@ -209,55 +357,109 @@ void Dump_list (list_t *list, FILE *file) {
 141) ┌
 142) └
 143) ┘
-*/
+*/  // Сначала в файл
     __uint8_t length_number = 3;
-
+    lst::num_t num = list->size;
     //fprintf (file, "%4c│", ' ');
     fprintf (file, "  N │");
 
-    for (__uint16_t i = 0; i < list->size; i++)
+    for (__uint16_t i = 0; i < num; i++)
         fprintf (file, "%3d |", i);
 
     fprintf (file, " list->head%5d", list->head);
 
     fprintf (file, "\n────┼");
-    for (__uint16_t i = 0; i < list->size - 1; i++)
+    for (__uint16_t i = 0; i < num - 1; i++)
         fprintf (file, "────┼");
     fprintf (file, "────┤\n");
 
-    // data
-    fprintf (file, "data|");
-    for (__uint16_t i = 0; i < list->size; i++)
+    fprintf (file, "data|");                            //data
+    for (__uint16_t i = 0; i < num; i++)
         fprintf (file, "%4d|", list->data[i]);
 
     fprintf (file, " list->tail%5d", list->tail);
     fprintf (file, "\n────┼");
-    for (__uint16_t i = 0; i < list->size - 1; i++)
+    for (__uint16_t i = 0; i < num - 1; i++)
         fprintf (file, "────┼");
     fprintf (file, "────┤\n");
 
-    // next
-    fprintf (file, "next|");
-    for (__uint16_t i = 0; i < list->size; i++)
+    fprintf (file, "next|");                            // next
+    for (__uint16_t i = 0; i < num; i++)
         fprintf (file, "%4d|", list->next[i]);
-
 
     fprintf (file, " list->free%5d", list->free);
     fprintf (file, "\n────┼");
-    for (__uint16_t i = 0; i < list->size - 1; i++)
+    for (__uint16_t i = 0; i < num - 1; i++)
         fprintf (file, "────┼");
     fprintf (file, "────┤\n");
 
-    // prev
-    fprintf (file, "prev|");
-    for (__uint16_t i = 0; i < list->size; i++)
+    fprintf (file, "prev|");                            //prev
+    for (__uint16_t i = 0; i < num; i++)
         fprintf (file, "%4d|", list->prev[i]);
 
     fprintf (file, " list->count%4d", list->count);
     fprintf (file, "\n────┼");
-    for (__uint16_t i = 0; i < list->size - 1; i++)
+    for (__uint16_t i = 0; i < num - 1; i++)
         fprintf (file, "────┴");
     fprintf (file, "────┘\n");
 
     fprintf (file, "\n");
+    ////////////////////////////////////////////////    //DOT
+
+    if (lst::dmp_DOT_ON)
+        Dump_list_dot (list);
+
+}
+
+void Dump_list_dot (list_t *list) {
+    static size_t number_calls = 0;
+    static bool first = false;
+
+    if (!first) {
+        system ("rm -rf dot_dir/ 2> /dev/null");
+        system ("mkdir dot_dir");
+        system ("touch dot_dir/README.txt; echo This folder constatly deleted! > dot_dir/README.txt");
+        first = true;
+    }
+
+    FILE *file_dot = fopen ("temp_file.dot", "w");
+    if (file_dot == nullptr) {
+        PRINT_ERROR ("DONT CREATE FILE_DOT")
+        return;
+    }
+
+    fprintf (file_dot, "digraph G {\n"
+                       "    rankdir = LR;\n"
+                       "    node[shape=ellipse, fontsize=50, color = red];\n"
+                       "    edge[fontsize=50, color = blue, fillcolor = blue];\n");
+    lst::num_t num = 0;
+    lst::num_t size = list->size;
+
+    for (num = 0; num < size; num++)
+        fprintf (file_dot, "\t%d [label = \"%d\"];\n", num, list->data[num]);
+
+    fprintf (file_dot, "head -> %d[label = \"%d\"];\n", list->head, list->head);
+    for (num = list->next[list->head]; num != list->tail; num = list->next[num])
+        fprintf (file_dot, "\t%d -> %d[label = \"%d\"]\n", list->prev[num], num, list->prev[num]);
+    fprintf (file_dot, "\t%d -> %d[label = \"%d\"]\n", list->prev[num], num, list->prev[num]);
+    fprintf (file_dot, "\t%d -> tail[label = \"%d\"]\n", num, list->tail);
+
+    fprintf (file_dot, "free -> %d[label = \"%d\"];\n", list->free, list->free);
+    for (num = list->free; num < size - 1; num = list->next[num])
+        fprintf (file_dot, "\t%d -> %d[label = \"%d\"]\n", num, list->next[num], list->next[num]);
+
+    fprintf (file_dot, "\t%d [label = \"%d\"];\n", size, list->count);
+    fprintf (file_dot, "count -> %d\n", size);
+
+    fprintf (file_dot, "}");
+    fclose (file_dot);
+
+    char comand[200] = "";
+    sprintf (comand, "dot -Tpng -Gsize=%zu,16\\! -Gdpi=100 temp_file.dot -o dot_dir/%zu.png", 10+number_calls, number_calls);
+    if (lst::dmp_DOT_SHOW_AT_ONCE)
+        sprintf (comand, "%s; fim -a dot_dir/%zu.png", comand, number_calls);
+    //printf ("%s", comand);
+    system (comand);
+
+    number_calls++;
 }
